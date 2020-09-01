eaefbb2e63f4d183d5c886ceb234cddd4776c700
Joshua Brewer <jtb91594@gmail.com>
2020-09-01 17:40:51 -0400

    updated gitignore and cleaned up yaml_decode version of terraform code

--- yaml_decode.tf
+++ yaml_decode.tf
@@ -10,44 +10,38 @@ locals {
   group_app_service_vals = [for config_val in local.yaml_vals : [config_val.app_code, config_val.group_code, config_val.service_name]]
 }
 
-# resource "bitbucket_project" "group_proj" {
-#   for_each   = toset([for config_val in local.yaml_vals : config_val.group_code])
-#   owner      = var.username
-#   name       = each.value
-#   key        = "${upper(each.value)}PROJ"
-#   is_private = true
-# }
+resource "bitbucket_project" "group_proj" {
+  for_each   = toset([for config_val in local.yaml_vals : config_val.group_code])
+  owner      = var.username
+  name       = each.value
+  key        = "${upper(each.value)}PROJ"
+  is_private = true
+}
 
-# resource "bitbucket_repository" "app_repo" {
-#   count       = length(local.group_app_set)
+resource "bitbucket_repository" "app_repo" {
+  count       = length(local.group_app_set)
+  owner       = var.username
+  name        = local.group_app_set[count.index][0]
+  project_key = "${upper(local.group_app_set[count.index][1])}PROJ"
+  is_private  = true
+}
+
+# resource "bitbucket_repository" "test_repo" {
 #   owner       = var.username
-#   name        = local.group_app_set[count.index][0]
-#   project_key = "${upper(local.group_app_set[count.index][1])}PROJ"
+#   name        = local.group_app_set[0][0]
+#   project_key = "${upper(local.group_app_set[0][1])}PROJ"
 #   is_private  = true
 
 #   provisioner "file" {
-#     source = "README.md"
-#     #destination = "${local.group_app_set[count.index][0]}/${coalesce([for pod in local.group_app_service_vals : pod[2] if pod[0] == local.group_app_set[count.index][0] && pod[1] == local.group_app_set[count.index][1]])}"
-#     destination = "README.md"
+#     source      = "README.md"
+#     destination = "${local.group_app_set[0][0]}/README.md"
+
+#     connection {
+#       type     = "winrm"
+#       https    = "true"
+#       user     = var.username
+#       password = var.password
+#       host     = "https://jtb91594@bitbucket.org/jtb91594/${local.group_app_set[0][0]}.git"
+#     }
 #   }
 # }
-
-resource "bitbucket_repository" "test_repo" {
-  owner       = var.username
-  name        = local.group_app_set[0][0]
-  project_key = "${upper(local.group_app_set[0][1])}PROJ"
-  is_private  = true
-
-  provisioner "file" {
-    source      = "README.md"
-    destination = "${local.group_app_set[0][0]}/README.md"
-
-    connection {
-      type     = "winrm"
-      https    = "true"
-      user     = var.username
-      password = var.password
-      host     = "https://jtb91594@bitbucket.org/jtb91594/${local.group_app_set[0][0]}.git"
-    }
-  }
-}
